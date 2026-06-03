3fa1dcd255ed8dff9c93d26c681482b347d5b5d4
Afiqah076796 <076796@putra.unisza.edu.my>
2026-06-03 14:10:59 +0800

    Initial portfolio website setup

---
+++ script.js
@@ -0,0 +1,41 @@
+// =========================
+// DARK MODE
+// =========================
+
+function darkMode(){
+
+    document.body.classList.toggle("dark-mode");
+
+}
+
+// =========================
+// SCROLL FADE ANIMATION
+// =========================
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold: 0.15
+});
+
+const hiddenElements = document.querySelectorAll(
+'section, .card, .project-card, .timeline-card, .contact-card, .stat-box, .about-intro-card, .contact-form'
+);
+
+hiddenElements.forEach((el) => {
+
+    el.classList.add("hidden");
+
+    observer.observe(el);
+
+});
